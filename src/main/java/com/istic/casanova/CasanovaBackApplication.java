2134ea51324f321f870cc9d8560c278a444bdab9
Mariem <mariem.rouiissi@gmail.com>
2021-01-18 13:26:36 +0100

    init commit

---
+++ src/main/java/com/istic/casanova/CasanovaBackApplication.java
@@ -0,0 +1,13 @@
+package com.istic.casanova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasanovaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasanovaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/istic/casanova/CasanovaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.istic.casanova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasanovaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
